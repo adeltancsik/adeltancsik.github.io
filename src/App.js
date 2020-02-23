ed736710dc1ac4c36cb7b69ef1741a519bbc9393
adeltancsik <tancsikadel@gmail.com>
2020-02-23 12:21:22 +0100

    Spinner on page load

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import Intro from "./components/Intro";
+import $ from "jquery";
 
 class App extends Component {
+  componentDidMount() {
+    // Play initial animations on page load, then stop.
+    $(document).ready(function() {
+      $(window).on("load", function() {
+        window.setTimeout(function() {
+          $("#spinner").removeClass("lds-dual-ring");
+        }, 100);
+      });
+    });
+  }
   render() {
     return (
       <div id="app">
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 export default class Intro extends Component {
-  c;
   render() {
     return (
       <div id="intro">
         <h1>Welcome to my portfolio!</h1>
+        <div className="lds-dual-ring" id="spinner"></div>
       </div>
     );
   }

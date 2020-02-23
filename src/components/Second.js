e1fd86847248bec485ecfe465d04403ce0da25fc
adeltancsik <tancsikadel@gmail.com>
2020-02-23 22:51:57 +0100

    Colors, content, typography, positioning

--- src/components/Second.js
+++ src/components/Second.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import pic2 from "../assets/images/Adel.jpg";
 
 export default class Second extends Component {
   render() {
     return (
-      <section id="second" className="panel">
-        <div className="span-6">
-          <h2>About me</h2>
+      <section id="second" className="panel spotlight right">
+        <div className="content right span-7">
+          <div className="medium">
+            <h1 class="major">About me</h1>
+            <blockquote>
+              “Change is inevitable. Growth is optional.”
+              <br />
+              <span>— John Maxwell</span>
+            </blockquote>
+            <p>
+              This quote describes my beliefs since I decided to change career
+              from a Physiotherapist to a Web Developer.
+              <br />
+              A journey that led me to complete a coding bootcamp in Amsterdam
+              at the end of last year after a period of autodidactism.
+              <br />
+              An adventure in which I found my passion for coding.
+              <br />
+              A path that fills me up with excitement and curiosity for where it
+              will take me next.
+              <br />
+              Currently, I'm at the milestone of looking for my first job as a
+              Full-stack Web Developer in the Netherlands.
+            </p>
+          </div>
+        </div>
+        <div class="image">
+          <img src={pic2} alt="Photo of me 2." />
         </div>
       </section>
     );

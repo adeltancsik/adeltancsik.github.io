4a82c70277fd256dd1f2972c93585fbdb00e1b55
adeltancsik <tancsikadel@gmail.com>
2020-03-02 16:34:34 +0100

    Ready to be deployed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // don't comment this out in development mode! only when it's ready to be deployed. + change href from style.min.css to style.css in dev mode + npm run watch:sass
-import "./assets/css/icon-font.css";
-import "./assets/css/style.css";
+// import "./assets/css/icon-font.css";
+import "./assets/css/style.min.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
